c3b04be6cc6e3cf65c6629203eb7b3ccf705aaf3
swatikajikar <swatikajikar12@gmail.com>
2026-06-24 16:28:45 +0530

    Added

---
+++ Controllers/StateApiController.cs
@@ -0,0 +1,27 @@
+﻿using Core_APIsWithJenkinsPipeline.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_APIsWithJenkinsPipeline.Controllers
+{
+    //[Route("api/[controller]")]
+    [ApiController]
+    public class StateApiController : ControllerBase
+    {
+        [HttpGet]
+        [Route("api/state")]
+        public List<State> GetStates() {
+            List<State> lst = new List<State>();
+            lst.Add(new State { StateId = 1,StateName="Maharashtra"});
+            lst.Add(new State { StateId = 2, StateName = "Goa" });
+            lst.Add(new State { StateId = 3, StateName = "Panjab" });
+            lst.Add(new State { StateId = 4, StateName = "Gujrat" });
+            lst.Add(new State { StateId = 5, StateName = "Kerala" });
+            lst.Add(new State { StateId = 6, StateName = "Hariyana" });
+            lst.Add(new State { StateId = 7, StateName = "Assam" });
+            lst.Add(new State { StateId = 8, StateName = "Bihar" });
+            return lst;
+
+        }
+    }
+}
---
+++ Models/State.cs
@@ -0,0 +1,8 @@
+﻿namespace Core_APIsWithJenkinsPipeline.Models
+{
+    public class State
+    {
+        public int StateId { get; set; }
+        public string StateName { get; set; }
+    }
+}
